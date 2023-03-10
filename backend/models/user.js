92b0572aff15672b152ac5256a07b4f9cd508a68
Ashleywashley3 <ash.mcmaken@gmail.com>
2023-03-09 19:25:24 -0500

    initial commit

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,9 +1,9 @@
 'use strict';
 const {
   Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+    } = require('sequelize');
+    module.exports = (sequelize, DataTypes) => {
+    class User extends Model {
 
     static associate({ Comment }) {
       User.hasMany(Comment, { as: 'author', foreignKey: 'author_id' })
@@ -19,11 +19,15 @@ module.exports = (sequelize, DataTypes) => {
     },
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {
+    email: DataTypes.STRING,
+    passwordDigest: DataTypes.STRING
+  }, 
+
+    {
     sequelize,
     underscored: true,
     modelName: 'User',
   });
+
   return User;
 };
